53a90a2f29a39199dfddbd6e86fc074e17c72778
Kristoffer Davidsen <nesdivad@protonmail.com>
2023-12-11 22:28:31 +0100

    Init

---
+++ Optimizely/Get-EpiMysitemediaSaslink.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [Parameter()]
+    [string]$ProjectId,
+
+    [Parameter()]
+    [string]$ClientKey,
+
+    [Parameter()]
+    [string]$ClientSecret
+)
+
+Connect-EpiCloud -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId
+$mysitemedia = Get-EpiStorageContainerSasLink -Environment "Integration" -StorageContainer "mysitemedia"
+
+If ($mysitemedia -ne $null -and $mysitemedia -ne '')
+{
+    Write-Host $mysitemedia
+}
+Else
+{
+    Write-Host "Can't produce SAS link..."
+}
+
---
+++ Optimizely/Import-EpiDatabase.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [Parameter()]
+    [string]$ProjectId,
+
+    [Parameter()]
+    [string]$ClientKey,
+
+    [Parameter()]
+    [string]$ClientSecret,
+
+    [Parameter()]
+    [string]$Path,
+
+    [Parameter()]
+    [string]$PackageName
+)
+
+Connect-EpiCloud -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId
+$saslink = Get-EpiDeploymentPackageLocation
+Add-EpiDeploymentPackage -SasUrl $saslink -Path $Path
+Start-EpiDeployment -DeploymentPackage @('$PackageName') -TargetEnvironment "Integration" -DirectDeploy
